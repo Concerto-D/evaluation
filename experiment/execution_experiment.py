2153073a2b92476e8a4db8a648f222fc93d09181
aomond-imt <antoine.omond@irisa.fr>
2022-07-03 09:31:08 +0200

    Test experiment 2

--- experiment/execution_experiment.py
+++ experiment/execution_experiment.py
@@ -252,7 +252,7 @@ def create_and_run_sweeper(version_concerto_name, is_normal, uptimes_to_test, tr
         "uptimes": uptimes_to_test,
         "transitions_times": transitions_times_list,
         "cluster": ["uvb"],
-        "experiment_num": [1]
+        "experiment_num": [2]
     })
     log.debug("--- All experiments to treat: ---")
     for k in sweeps:
