3acd933af31b547d51dbc36b3620d183f6e150af
aomond <antoine.omond@imt-atlantique.fr>
2023-02-18 21:14:32 +0100

    Fix bug check_sweeper_state

--- experiment/check_sweeper_state.py
+++ experiment/check_sweeper_state.py
@@ -5,13 +5,16 @@ from experiment import globals_variables, execution_experiment, experiment_contr
 if __name__ == "__main__":
     # Get the sweeper state according to the expe_name present in the expe_config_file
     configuration_expe_file_path = sys.argv[1]
-    global_params, reservation_params, sweeper_params = execution_experiment.extract_parameters(configuration_expe_file_path)
+    global_params, reservation_params, email_params, sweeper_params = execution_experiment.extract_parameters(configuration_expe_file_path)
     (
         expe_name,
         environment,
         version_concerto_d,
         all_expes_dir,
-        all_executions_dir
+        all_executions_dir,
+        _,
+        _,
+        _
     ) = global_params.values()
     log_experiment.initialize_logging(expe_name, stdout_only=True)
     globals_variables.all_expes_dir = all_expes_dir
