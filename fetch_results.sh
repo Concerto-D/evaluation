3e39cd8bc1936ecbc7b718f1dacc609688ff8a73
aomond <antoine.omond@imt-atlantique.fr>
2023-03-02 13:52:05 +0100

    Local changes

--- fetch_results.sh
+++ fetch_results.sh
@@ -1,12 +1,20 @@
 set -e  # Exit program on error
 
-if [[ -z "$2" ]]; then
+# $1: g5k site
+# $2: src_dir
+# $3: dst_dir
+
+if [[ -z "$3" ]]; then
   echo "need args"
   exit
 fi
-
-ssh anomond@$1.grid5000.fr 'cd experiments_results/; tar -cf experiments_results.tar *'
-scp anomond@$1.grid5000.fr:experiments_results/experiments_results.tar "/home/aomond/experiments_results/"
-tar --exclude=.ipynb_checkpoints -C "/home/aomond/experiments_results/$2" -xvf "/home/aomond/experiments_results/experiments_results.tar"
+echo 1
+ssh anomond@$1.g5k "cd experiments_results; tar -cf experiments_results.tar $2"
+echo 2
+scp anomond@$1.g5k:"experiments_results/experiments_results.tar" "/home/aomond/experiments_results"
+echo 3
+tar --exclude=.ipynb_checkpoints -C "/home/aomond/experiments_results/$3" -xvf "/home/aomond/experiments_results/experiments_results.tar"
+echo 4
 rm "/home/aomond/experiments_results/experiments_results.tar"
-#ssh anomond@$1.grid5000.fr 'rm -rf experiments_results/*'
+echo 5
+#ssh anomond@$1.g5k 'rm -rf experiments_results/*'
