26f188a3332eea7500ceb1730b74f8b2ff33da1f
aomond <antoine.omond@imt-atlantique.fr>
2023-05-04 13:26:46 +0200

    Try the same reconfiguration but with the dependencies only knowing about the server and not each others

--- synthetic_use_case/parallel_deps/assemblies/dep_assembly.py
+++ synthetic_use_case/parallel_deps/assemblies/dep_assembly.py
@@ -6,10 +6,10 @@ class DepAssembly(Assembly):
     def __init__(self, p, reconf_config_dict, waiting_rate, version_concerto_d, reconfiguration_name, nb_concerto_nodes):
         remote_assemblies = {"server_assembly": "server"}
 
-        # Adding remote components and assemblies
-        for i in range(nb_concerto_nodes):
-            if i != p:  # Not adding self
-                remote_assemblies[f"dep_assembly_{i}"] = f"dep{i}"
+        # # Adding remote components and assemblies
+        # for i in range(nb_concerto_nodes):
+        #     if i != p:  # Not adding self
+        #         remote_assemblies[f"dep_assembly_{i}"] = f"dep{i}"
 
         components_types = {
             "Dep": Dep
